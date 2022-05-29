53d9103cc989f8d1b35fc8e296e11d03797b0ed5
berkc <b.cal.2193@gmail.com>
2022-05-29 19:55:22 +0200

    29.05

--- src/test/java/pages/HotelMyCampPage.java
+++ src/test/java/pages/HotelMyCampPage.java
@@ -3,23 +3,24 @@ package pages;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class HotelMyCampPage {
     public HotelMyCampPage() {
-
+        PageFactory.initElements(Driver.getDriver(), this);
     }
-    //             (//a[@class='nav-link'])[7]
+
     @FindBy(xpath = "//*[text()='Log in']")
     public WebElement logInButton;
 
-    @FindBy (xpath = "//input[@name='UserName']")
+    @FindBy(xpath = "//input[@name='UserName']")
     public WebElement usernameBox;
 
     @FindBy(xpath = "//input[@name='Password']")
-    public  WebElement passwordBox;
+    public WebElement passwordBox;
 
-    @FindBy (xpath = "//input[@id='btnSubmit']")
+    @FindBy(xpath = "//input[@id='btnSubmit']")
     public WebElement logInButton2;
 
 }
